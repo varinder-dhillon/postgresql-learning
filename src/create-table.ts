18bdf759201b1133e7384889a0f629bb89ab4fa7
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-05-22 23:37:43 +0530

    Udemy Learning (Sec-1): Create table query

--- src/create-table.ts
+++ src/create-table.ts
@@ -1,34 +1,26 @@
 import { getClient } from "./utils";
 
 async function createTable() {
-    const createUserTableQuery = `
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
-        );
-    `;
+    try {
+        const client = await getClient();
+    
+        const createCitiesQuery = `
+            CREATE TABLE cities (
+                name VARCHAR(55),
+                country VARCHAR(55),
+                population INTERGER,
+                area INTERGER
+            )
+        `
 
-    const client = await getClient();
+        const queryResult = await client.query(createCitiesQuery)
 
-    await client.query(createUserTableQuery);
+        console.log("Query Executed Successfully! ->", queryResult);
+    } catch (error:any) {
+        console.log("Error message -> "+error.message)
+    }
+   
 
-    const createTodosQuery = `
-        CREATE TABLE todos (
-            id SERIAL PRIMARY KEY,
-            title TEXT NOT NULL,
-            description TEXT,
-            user_id INTEGER REFERENCES users(id),
-            done BOOLEAN DEFAULT FALSE
-        );
-    `;
-
-
-    await client.query(createTodosQuery);
-
-    console.log("Table created successfully!");
 }
-
-
 
 createTable();
