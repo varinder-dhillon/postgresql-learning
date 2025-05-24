e5fe63718ebe825f7b9989cf53ca5443f7e5cfd8
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-05-24 14:21:44 +0530

    Learned (UPDATE): Learnded the how to UPDATE the columns in tables

--- src/update-data.ts
+++ src/update-data.ts
@@ -1,13 +1,18 @@
 import { getClient } from "./utils";
 
-async function updateTodo(todoId: number) {
+async function updateCity() {
     const client = await getClient();
     
-    const updateTodoText = 'UPDATE todos SET done = $1 WHERE id = $2';
-    await client.query(updateTodoText, [true, todoId]);
+    // const updateTodoText = 'UPDATE todos SET done = $1 WHERE id = $2';
+    // await client.query(updateTodoText, [true, todoId]);
     
-    console.log(`Todo with ID ${todoId} updated to done!`);
+    // console.log(`Todo with ID ${todoId} updated to done!`);
+
+    const updateCityQuery = `UPDATE cities SET population = 39505000 WHERE name = 'Tokyo' RETURNING *`
+    const result = await client.query(updateCityQuery);
+
+    console.log('Query Executed -> ', result.rows[0])
 }
 
-const todoIdToUpdate = 1;
-updateTodo(todoIdToUpdate);
+// const todoIdToUpdate = 1;
+updateCity();
