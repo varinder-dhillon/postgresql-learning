0463a1ba0bad59524fed13863c5c207e294eeff5
Varinder Singh <varinderdhilloninfo@gmail.com>
2025-05-24 14:27:54 +0530

    Learned (DELETE): Learnded the how to DELETE the rows from tables

--- src/delete-data.ts
+++ src/delete-data.ts
@@ -1,13 +1,18 @@
 import { getClient } from "./utils";
 
-async function deleteTodo(todoId: number) {
+async function deleteCity() {
     const client = await getClient();
     
-    const deleteTodoText = 'DELETE FROM todos WHERE id = $1';
-    await client.query(deleteTodoText, [todoId]);
+    // const deleteTodoText = 'DELETE FROM todos WHERE id = $1';
+    // await client.query(deleteTodoText, [todoId]);
     
-    console.log(`Todo with ID ${todoId} deleted!`);
+    // console.log(`Todo with ID ${todoId} deleted!`);
+    const deleteCityQuery = `DELETE FROM cities WHERE name = 'Sao Paulo' RETURNING *`;
+    const result = await client.query(deleteCityQuery);
+
+    console.log("Deleted City -> ", result.rows[0]);
+    
 }
 
-const todoIdToDelete = 1;
-deleteTodo(todoIdToDelete);
+// const todoIdToDelete = 1;
+deleteCity();
